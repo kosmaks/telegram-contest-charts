474cfc98fef346d908b77b96725df4ff81b0acd5
Maxim Kostuchenko <kstmaks@gmail.com>
2019-03-23 18:20:38 +0300

    Fix hover line

--- src/telechart/canvas-renderer.js
+++ src/telechart/canvas-renderer.js
@@ -155,10 +155,7 @@ export class CanvasRenderer {
     const cx = this.xToScreen(frame.primaryAxis.data[idx]);
     ctx.beginPath();
     ctx.moveTo(cx, frame.paddingTop * DPR);
-    ctx.lineTo(
-      cx,
-      canvas.height - (frame.paddingBottom + frame.paddingTop) * DPR
-    );
+    ctx.lineTo(cx, canvas.height - frame.paddingBottom * DPR);
     ctx.globalAlpha = frame.darkTheme ? 0.1 : 1;
     ctx.strokeStyle = frame.darkTheme ? "#FFFFFF" : "#F0F0F0";
     ctx.lineWidth = DPR;
